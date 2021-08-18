21942e5acf4056fff1b9430255042e2f829929c7
Boboassa <uca.baldassi99@gmail.com>
2021-08-18 10:11:12 +0200

    Change scripts and script location

--- blocks.h
+++ blocks.h
@@ -1,16 +1,22 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+/* Modify this file to change what commands output to your statusbar
+ * and recompile using the make command. */
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		                     /*Update Interval*/ 	/*Update Signal*/
-    { "",       "$HOME/.local/bin/battery.sh",       60,                     0, },
-    { "",       "$HOME/.local/bin/backlight.sh",     0,                      1, },
-    { "",       "$HOME/.local/bin/volume.sh",        0,                      2, },
-    { "",       "$HOME/.local/bin/time.sh",          60,                     0, },
-//    { "",       "$HOME/.config/dwmblocks-scs/date.sh",          300,                     0, },
+/* Icon      Command                                      Interval     Signal*/
+    { "",     "$HOME/.local/bin/dwmblocks/db-battery",     0,           7, },
+    { "",     "$HOME/.local/bin/dwmblocks/db-backlight",   0,           8, },
+    { "",     "$HOME/.local/bin/dwmblocks/db-volume",      0,           9, },
+    { "",     "$HOME/.local/bin/dwmblocks/db-time",        30,          0, },
+//    { "",     "$HOME/.config/dwmblocks-scs/db-date",       300,         0, },
 
-	// {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	// {"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+/* Update the text by running the commands by running:
+ * kill -NUM $(pidof dwmblocks)
+ * with NUM = Signal + 34
+ * Every update signal must be different */
+
+
+/* Set delimeter between status commands
+ * NULL character ('\0') means no delimeter */
 static char delim[] = "  ";
 static unsigned int delimLen = 5;
