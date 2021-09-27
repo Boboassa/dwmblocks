fe7085d748d2d2b17f80a0b8a29e358d9eb020a3
Boboassa <uca.baldassi99@gmail.com>
2021-09-27 12:27:50 +0200

    Add wifi status script

--- blocks.h
+++ blocks.h
@@ -2,6 +2,7 @@
  * and recompile using the make command. */
 static const Block blocks[] = {
 /* Icon      Command                                      Interval     Signal*/
+    { "",     "$HOME/.local/bin/dwmblocks/db-wifi",        10,          6, },
     { "",     "$HOME/.local/bin/dwmblocks/db-battery",     0,           7, },
     { "",     "$HOME/.local/bin/dwmblocks/db-backlight",   0,           8, },
     { "",     "$HOME/.local/bin/dwmblocks/db-volume",      0,           9, },
